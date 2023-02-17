1eb4484f5843424058a4d4335da09a1dbcf79301
ZacharyACummings <zachary.a.cummings@gmail.com>
2023-02-17 04:46:47 -0800

    New program

---
+++ 99_bottles.py
@@ -0,0 +1,9 @@
+for i in range(99):
+    print(f'{i} bottles of beer on the wall')
+    print('')
+    print(f'{i} bottles of beer')
+    print('')
+    print(f'Take one down, pass it around')
+    print('')
+    print(f'{i-1} bottles of beer on the wall')
+    print('')
